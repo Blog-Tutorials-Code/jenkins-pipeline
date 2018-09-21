e33e6d243a49d7b1a5dad9ee34cd88d2be318447
Ferdinand Agyei-Yeboah <ferdinandyeboah@gmail.com>
2018-09-21 14:54:20 -0400

    Removing old images on docker build and adding stage to push to dockerhub

--- pipeline-scripts/spring-playground.groovy
+++ pipeline-scripts/spring-playground.groovy
@@ -1,6 +1,6 @@
 // Some global variables I have to figure out the best place to put or read them from
-def GCLOUD_PROJECT = "[SUPPLY_PROJECT_NAME]"
-def REGION = "[SUPPLY_REGION]"
+def GCLOUD_PROJECT = "sharp-terminal-215500"
+def REGION = "us-east1-b"
 def VM_INSTANCE = "spring-playground"
 
 pipeline {
@@ -34,10 +34,10 @@ pipeline {
                 sh "gcloud version"
                 sh "docker version"
 
-                // Configure the GCLOUD CLI
+                // Configure the GCLOUD CLI. Setting project and region should be okay.
                 sh "gcloud auth activate-service-account --key-file $GCLOUD_SERVICE_ACCOUNT_KEY"
                 sh "gcloud config list" 
-                // sh "gcloud config set project ${REGION}" //
+                // sh "gcloud config set project ${GCLOUD_PROJECT}" //
             }
         }
 
@@ -50,21 +50,54 @@ pipeline {
 
         stage ("Docker Build"){
             steps {
+                // Remove old application docker image. NOTE: Can remove all images using: docker rmi $(docker images -q)
+                sh "docker image rm ${params.ARTIFACT_NAME}"
+                // Build the application docker image
                 sh "docker build . -t ${params.ARTIFACT_NAME}"
             }
         }
 
-        // TODO: Push docker image to nexus repo.
-        // stage ("Push to Artifact Repo"){
+        stage ("Push to Dockerhub"){
+            environment {
+                DOCKER_HUB_USERNAME = credentials("DOCKER_HUB_USERNAME")
+                DOCKER_HUB_PASSWORD = credentials("DOCKER_HUB_PASSWORD")
+            }
+
+            steps {
+                sh "docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD"
+                sh "docker tag ${params.ARTIFACT_NAME} ferdinandyeboah/${params.ARTIFACT_NAME}" 
+                sh "docker push ferdinandyeboah/${params.ARTIFACT_NAME}" 
+            }
+        }
+
+        // TODO: Instead of pushing to dockerhub, push to nexus
+        // stage ("Push to Nexus Repo"){
         //     steps {
-        //         sh "docker build . -t ${params.ARTIFACT_NAME}"
+                // docker push...
         //     }
         // }
         
 
         // stage ("Deploy to GCP"){
         //     steps {
-                // Whatever GCP deploy command with the latest image
+        //         // Deploy. Don't forget to set project, necessary firewall rules etc. Maybe should use update-container since ALB already set on the spring-playground vm.
+        //         // I believe I have two options. Update the instance unmanaged group container, or update the vm instance directly. Had to create an instance group because of the LB.
+        //         // Update: The above is incorrect, I created an UNMANAGED instance group (not based on a template) so I have to update the instance directly
+        //         // What to do about first case where container not deployed and should be created and afterwards should be updated?
+        //         // LATER: DEPLOY all infrastructure as code (i.e ALB). Does google have something like AWS CloudFormation?
+
+        //         //Seems like docker image must be remotely hosted. Cannot be on local machine and referenced in --container-image
+        //         sh " gcloud compute instances create-with-container [INSTANCE_NAME] \
+        //             --container-image [DOCKER_IMAGE]"
+
+        //         // Add appropiate firewall tags. Add the project flag as well
+        //         sh "gcloud compute instances add-tags [INSTANCE-NAME] \
+        //             --zone [ZONE] \
+        //             --tags [TAGS]"
+
+
+
+        //         //Could try catch and create-with-container if get an error then use update-with-container
         //     }    
         // }
 
